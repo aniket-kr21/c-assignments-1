1961d27bc4930d6876673d5398a5fd7d92648e5a
Aniket Kumar <118657207+aniket-kr21@users.noreply.github.com>
2022-11-23 22:36:44 +0530

    Add files via upload

---
+++ Assignment.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float r,x;
+printf("Enter the radius of the circle:",r);
+scanf("%f",&r);
+x=2*3.14*r;
+printf("Area of circle is %.2f sq. unit having the radius %.0f unit.",x,r);
+return 0;
+}
